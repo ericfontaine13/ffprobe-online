508dce722affc259b7448cb2bb0b5446ae9bf1a5
ericfontaine13 <ericfontaine13@gmail.com>
2026-03-07 10:06:45 +0100

    Initialize Next.js project

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        Starting coding app
+      </h1>
+    </main>
+  );
+}
